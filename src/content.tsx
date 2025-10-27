87f49399ecb426f8d4dca6ffc1d9a33f0f6233be
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2025-10-27 20:44:54 +0900

    Use mutation observer as its SPA

--- src/content.tsx
+++ src/content.tsx
@@ -2,35 +2,63 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { loadWorkflows } from "./loadWorkflows";
 
+const renderApp = async (
+  original: HTMLElement & { parentElement: HTMLElement }
+) => {
+  if (original.dataset._github_actions_fuzzy_search_extension) {
+    return;
+  }
+
+  original.dataset._github_actions_fuzzy_search_extension = "true";
+
+  const match = window.location.href.match(
+    /https:\/\/github.com\/(.*?)\/(.*?)\/actions/
+  );
+  if (!match) {
+    return;
+  }
+  const org = match[1];
+  const repo = match[2];
+
+  const host = document.createElement("div");
+  original.parentElement.prepend(host);
+  original.style.display = "none";
+
+  const appRoot = document.createElement("div");
+
+  host.appendChild(appRoot);
+
+  const workflows = await loadWorkflows({ org, repo });
+  createRoot(appRoot).render(<App workflows={workflows} />);
+};
+
+const SELECTOR = "actions-workflow-list nav-list-group";
+
 const main = async () => {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.type === "childList") {
+        mutation.addedNodes.forEach((node) => {
+          if (node instanceof HTMLElement) {
+            const elem = node.matches(SELECTOR)
+              ? node
+              : node.querySelector(SELECTOR);
+            if (elem && elem.parentElement) {
+              renderApp(elem as HTMLElement & { parentElement: HTMLElement });
+            }
+          }
+        });
+      }
+    });
+  });
+
   const original = document.querySelector<HTMLElement>(
     "actions-workflow-list nav-list-group"
   );
-
   if (original && original.parentElement) {
-    const host = document.createElement("div");
-    original.parentElement.prepend(host);
-    original.style.display = "none";
-
-    const appRoot = document.createElement("div");
-
-    host.appendChild(appRoot);
-    // Use shadow DOM?
-    // const shadow = host.attachShadow({ mode: "open" });
-    // shadow.appendChild(appRoot);
-
-    const match = window.location.href.match(
-      /https:\/\/github.com\/(.*?)\/(.*?)\/actions/
-    );
-    if (!match) {
-      return;
-    }
-    const org = match[1];
-    const repo = match[2];
-
-    const workflows = await loadWorkflows({ org, repo });
-    createRoot(appRoot).render(<App workflows={workflows} />);
+    renderApp(original as HTMLElement & { parentElement: HTMLElement });
   }
+  observer.observe(document.body, { childList: true, subtree: true });
 };
 
 main();
